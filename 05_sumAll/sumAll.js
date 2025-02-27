a0db0cb25cb654024ae0fe0eed60fb3637e4bb70
Josine-02 <josine.deleeuwvanweenen@gmail.com>
2025-02-27 15:10:14 +0100

    complete excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start <0 || end <0 || (start*10)%10 !==0 ||(end*10)%10 !==0 || typeof(start) !== 'number' || typeof(end) !== 'number'){
+        return 'ERROR'
+    }
 
+    let sum = start
+    let i = start +1
+    iterationEnd=end
+
+    if (start > end) {
+        sum = end
+        i = end +1
+        iterationEnd=start
+    }
+
+while (i<=iterationEnd){
+    sum += i;
+    i++;
+}
+return sum
 };
 
 // Do not edit below this line
