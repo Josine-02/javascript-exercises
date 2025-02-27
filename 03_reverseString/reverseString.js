04790213180ef8318d47486f5b80929f3da80816
Josine-02 <josine.deleeuwvanweenen@gmail.com>
2025-02-27 13:32:35 +0100

    complete excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+stringArray = string.split("")
+reverseStringArray =[]
 
+for (i=0; i<string.length;i++){
+    reverseStringArray.splice(0, 0, stringArray[i])
+}
+
+return reverseStringArray.join("")
 };
 
 // Do not edit below this line
